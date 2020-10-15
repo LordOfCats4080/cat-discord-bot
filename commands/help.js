633d8eefdab8f3771e2ce0c6d04aa30043210a5c
LordOfCats4080 <lordofcats4080@gmail.com>
2020-10-15 10:30:29 -0500

    Fix wrong variable name

--- commands/help.js
+++ commands/help.js
@@ -30,7 +30,7 @@ module.exports = {
 	const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
 	if (!command) {
-	    return message.reply('that\'s not a valid command!');
+	    return msg.reply('that\'s not a valid command!');
 	}
 
 	data.push(`**Name:** ${command.name}`);
